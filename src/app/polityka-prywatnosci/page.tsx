7611c4b36f5ab5c8388f644df4008e5816ab211e
KrzysztofHum <k.humienny1996@protonmail.com>
2025-03-21 12:51:54 +0100

    privacy policy

---
+++ src/app/polityka-prywatnosci/page.tsx
@@ -0,0 +1,353 @@
+import React from "react";
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="max-w-4xl mx-auto p-6 text-gray-800">
+      <h1 className="text-3xl font-bold text-center mb-6">
+        Polityka Prywatności
+      </h1>
+
+      <section className="mb-6">
+        <h2 className="text-xl font-semibold mb-3">Postanowienia Ogólne</h2>
+        <p>
+          1. Niniejsza Polityka prywatności opisuje sposób, w jaki postępujemy z
+          Danymi Osobowymi zbieranymi za pośrednictwem strony internetowej:{" "}
+          <a
+            href="https://prezentowe-emocje.pl"
+            className="text-blue-600 hover:underline"
+          >
+            https://prezentowe-emocje.pl
+          </a>
+          , a także Danymi Osobowymi zbieranymi za pośrednictwem poczty
+          elektronicznej (e-mail) lub innych środków komunikacji elektronicznej.
+        </p>
+        <p>
+          2. Administratorem Danych Osobowych użytkowników jest Krzysztof
+          Humienny. Twoje dane są przetwarzane zgodnie z RODO.
+        </p>
+        <p>
+          3. Twoje Dane Osobowe są przetwarzane zgodnie z Rozporządzeniem
+          Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016
+          r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych
+          osobowych i w sprawie swobodnego przepływu takich danych oraz
+          uchylenia dyrektywy 95/46/WE (ogólne rozporządzenie o ochronie
+          danych), zwanego w dalszej części: „RODO”.
+        </p>
+        <p>
+          4. Wszelkie określenia pisane wielką literą (np. Serwis, Dane Osobowe)
+          należy rozumieć zgodnie z ich definicją zawartą w Regulaminie
+          dostępnym na stronie https://prezentowe-emocje/regulamin/, chyba że w
+          treści niniejszej Polityki Prywatności wyraźnie zastrzeżono inaczej.
+        </p>
+      </section>
+
+      <section className="mb-6">
+        <h2 className="text-xl font-semibold mb-3">
+          PODSTAWY PRAWNE, CELE, ZASADY ORAZ CZAS PRZETWARZANIA TWOICH DANYCH W
+          SERWISIE
+        </h2>
+        <ol className="list-decimal list-inside space-y-2">
+          <li>
+            Administrator przetwarza Twoje Dane Osobowe w zakresie niezbędnym do
+            świadczenia poszczególnych usług oferowanych przez Serwis.
+          </li>
+          <li>
+            Twoje Dane Osobowe przetwarzane są przez Administratora:
+            <ol className="list-decimal list-inside pl-6 mt-2">
+              <li>
+                W celu świadczenia usług drogą elektroniczną w zakresie
+                sprzedaży detalicznej Towarów za pośrednictwem sieci Internet –
+                podstawą prawną przetwarzania Danych Osobowych jest niezbędność
+                przetwarzania do wykonania umowy (art. 6 ust. 1 lit. b RODO), w
+                tym przypadku przetwarzamy Dane Osobowe przez czas obowiązywania
+                umowy i do momentu upływu terminów do dochodzenia roszczeń;
+              </li>
+              <li>
+                W celu rozpatrywania skarg i reklamacji związanych z zawieranymi
+                Transakcjami – podstawą przetwarzania Danych Osobowych jest
+                niezbędność wypełnienia obowiązku prawnego ciążącego na
+                Administratorze (art. 6 ust. 1 lit. c RODO), w tym przypadku
+                przetwarzamy Dane Osobowe przez rok od upływu okresu rękojmi lub
+                zrealizowaniu przez Ciebie uprawnień z niej wynikających;
+              </li>
+              <li>
+                W celu ewentualnego ustalenia, dochodzenia lub obrony przed
+                roszczeniami – podstawą prawną przetwarzania jest uzasadniony
+                interes Administratora (tj. ochrona swoich praw; art. 6 ust. 1
+                lit. f RODO); w tym przypadku przetwarzamy Dane Osobowe do czasu
+                upływu terminów do dochodzenia roszczeń;
+              </li>
+              <li>
+                W celach analitycznych i statystycznych – podstawą prawną
+                przetwarzania jest uzasadniony interes Administratora (tj.
+                analiza aktywności osób korzystających z Serwisu, a także ich
+                preferencji w celu poprawy funkcjonalności Serwisu; art. 6 ust.
+                1 lit. f RODO), w tym przypadku Dane Osobowe są przetwarzane na
+                podstawie dodatkowej podstawy prawnej (np. pozwalającej na
+                przetwarzanie Danych Osobowych w celu wykonania umowy) – w razie
+                gdy Administrator utraci tą podstawę, Dane Osobowe zostaną
+                zanonimizowane;
+              </li>
+              <li>
+                W celu realizacji obowiązku ustawowego ciążącego na
+                Administratorze, wynikającego w szczególności z przepisów
+                podatkowych i przepisów o rachunkowości – podstawą prawną
+                przetwarzania jest obowiązek prawny (art. 6 ust. 1 lit. c RODO);
+                w tym przypadku Dane Osobowe są przetwarzane do czasu upływu
+                terminu przedawnienia zobowiązań podatkowych;
+              </li>
+              <li>
+                W celach marketingowych Administratora: – jeżeli działania
+                marketingowe prowadzone są bez wykorzystania środków komunikacji
+                elektronicznej, podstawą prawną przetwarzania jest uzasadniony
+                interes Administratora związany z promocją prowadzonej
+                działalności (art. 6 ust. 1 lit f RODO w zw. z motywem 43 RODO),
+                Dane Osobowe są przetwarzane do czasu zgłoszenia przez Ciebie
+                skutecznego sprzeciwu; – jeżeli działania marketingowe
+                prowadzone są z wykorzystaniem środków komunikacji
+                elektronicznej, podstawę prawną przetwarzania stanowi zgoda na
+                takie działania (art. 6 ust. 1 lit. a RODO oraz przepisy innych
+                aktów prawnych wymagających takiej zgody); Dane Osobowe są
+                przetwarzane do czasu cofnięcia przez Ciebie zgody.
+              </li>
+            </ol>
+          </li>
+          <li>
+            Administrator może przetwarzać Twoje następujące Dane Osobowe:
+            <ol className="list-decimal list-inside pl-6 mt-2">
+              <li>Imię i Nazwisko</li>
+              <li>Adres zamieszkania</li>
+              <li>Telefon</li>
+              <li>Adres E-mail</li>
+              <li>Adres IP</li>
+              <li>Data ślubu</li>
+            </ol>
+          </li>
+
+          <li>
+            Zakres wskazanych danych jest zgodny z zasadą minimalizacji. Brak
+            wskazania powyższych danych uniemożliwia korzystanie z usług
+            Serwisu. Po upływie okresu przetwarzania, Twoje Dane Osobowe są
+            niezwłocznie usuwane lub anonimizowane.
+          </li>
+          <li>
+            W związku z realizacją Transakcji, Twoje Dane Osobowe będą ujawniane
+            zewnętrznym partnerom, podmiotom współpracującym, odbiorcom, w
+            szczególności podmiotom odpowiedzialnym za obsługę systemów
+            informatycznych, podmiotom prowadzącym działalność kurierską lub
+            pocztową, bankom i operatorom płatności, podmiotom świadczącym
+            usługi księgowe, agencjom marketingowym oraz podmiotom powiązanym z
+            Administratorem. W przypadku zgłoszenia udzielania informacji
+            dotyczących osoby korzystającej z Serwisu przez właściwy organ lub
+            osobę trzecią, Administrator udostępni Twoje Dane Osobowe, jeżeli
+            taki obowiązek wynika z obowiązujących przepisów prawa.
+            Administrator nie przekazuje Twoich Danych Osobowych do państwa
+            trzeciego lub organizacji międzynarodowej spoza Europejskiego
+            Obszaru Gospodarczego.
+          </li>
+        </ol>
+      </section>
+
+      <section className="mb-6">
+        <h2 className="text-xl font-semibold mb-3">
+          PRZETWARZANIE DANYCH OSOBOWYCH W SPOSÓB ZAUTOMATYZOWANY
+        </h2>
+        <p>
+          Administrator nie przetwarza Twoich Danych Osobowych w sposób
+          zautomatyzowany (w tym w formie profilowania), z wyjątkiem informacji
+          zawartych w plikach cookies.
+        </p>
+        <h2 className="text-xl font-semibold mb-3">TWOJE UPRAWNIENIA</h2>
+        <ul className="list-disc pl-5 space-y-2">
+          <li>
+            Zgodnie z przepisami RODO, przysługują Ci następujące prawa związane
+            z kontrolą przetwarzania Twoich Danych Osobowych:
+            <ol className="list-decimal list-inside pl-6 mt-2">
+              <li>
+                prawo dostępu do treści Twoich Danych Osobowych (m. in.
+                uzyskania informacji jakiego rodzaju Dane Osobowe są
+                przetwarzane przez Administratora, otrzymania kopii swoich
+                Danych Osobowych);
+              </li>
+              <li>
+                prawo żądania poprawienia, uaktualnienia lub sprostowania Twoich
+                Danych Osobowych;
+              </li>
+              <li>
+                prawo żądania usunięcia Twoich Danych Osobowych, jeżeli są one
+                niekompletne, nieaktualne, nieprawdziwe lub zostały zebrane z
+                naruszeniem przepisów prawa albo są zbędne do realizacji celu,
+                dla którego zostały zebrane; niezależnie od powyższego
+                przysługuje ci możliwość usunięcia konta w Serwisie (nie jest to
+                jednak równoznaczne z usunięciem Twoich Danych Osobowych);
+              </li>
+              <li>
+                prawo wniesienia skargi do organu nadzorczego zajmującego się
+                ochroną danych osobowych – w Polsce jest nim Prezes Urzędu
+                Ochrony Danych Osobowych (np. w przypadku uznania, że
+                przetwarzanie Twoich Danych Osobowych narusza przepisy RODO lub
+                inne przepisy dotyczące ochrony Danych Osobowych);
+              </li>
+              <li>
+                prawo do żądania ograniczenia przetwarzania Twoich Danych
+                Osobowych;
+              </li>
+              <li>
+                prawo wniesienia sprzeciwu wobec przetwarzania Twoich Danych
+                Osobowych, jeżeli przetwarzanie następuje na podstawie
+                uzasadnionego interesu Administratora lub w celu marketingu
+                bezpośredniego.
+              </li>
+              <p>
+                Jeżeli Dane Osobowe są przetwarzane na podstawie Twojej zgody,
+                przysługuje Ci:
+              </p>
+              <li>prawo do wycofania zgody w dowolnej chwili;</li>
+              <li>prawo do przenoszenia Twoich Danych Osobowych.</li>
+            </ol>
+          </li>
+          <li>
+            W celu realizacji swoich uprawnień, skontaktuj się z Administratorem
+            za pośrednictwem kanałów komunikacji wskazanych w: „Dane
+            Kontaktowe”.
+          </li>
+        </ul>
+      </section>
+
+      <section className="mb-6">
+        <h2 className="text-xl font-semibold mb-3">Pliki Cookies</h2>
+        <ul className="list-disc pl-5 space-y-2">
+          <li>Serwis korzysta z informacji zawartych w plikach cookies.</li>
+          <li>
+            Pliki cookies stanowią dane informatyczne, w szczególności pliki
+            tekstowe, które przechowywane są w Twoim urządzeniu końcowym (np. w
+            pamięci komputera) i przeznaczone są do korzystania ze stron
+            internetowych Serwisu.
+          </li>
+          <li>
+            Pliki cookies są wykorzystywane w celu:
+            <ol className="list-decimal list-inside pl-6 mt-2">
+              <li>identyfikacji Twojej osoby jako zalogowanej w Serwisie;</li>
+              <li>zapamiętywania Towarów w Koszyku;</li>
+              <li>zapamiętywanie danych logowania do Serwisu;</li>
+              <li>
+                dostosowywania zawartości Serwisu do Twoich indywidualnych
+                preferencji (np. układ strony);
+              </li>
+              <li>
+                prowadzenie anonimowych statystyk pozwalających Administratorowi
+                poprawić funkcjonalność Serwisu.
+              </li>
+            </ol>
+          </li>
+          <li>
+            W ramach Serwisu stosowane są następujące rodzaje cookies: cookies
+            stałe, cookies sesyjne, reklamowe, funkcjonalne oraz związane z
+            bezpieczeństwem. Występują przypadki, w których oprogramowanie
+            służące do przeglądania stron internetowych (przeglądarka
+            internetowa) domyślnie dopuszcza przechowywanie plików cookies w
+            urządzeniu końcowym. Możliwe jest w każdym czasie dokonanie zmiany
+            ustawień dotyczących plików cookies. Ustawienia te mogą zostać
+            zmienione w szczególności w taki sposób, aby blokować automatyczną
+            obsługę plików cookies w ustawieniach przeglądarki internetowej bądź
+            informować o ich każdorazowym zamieszczeniu w urządzeniu.
+            Ograniczenia stosowania plików cookies mogą wpłynąć na niektóre
+            funkcjonalności dostępne w Serwisie. Pliki cookies wykorzystywane
+            mogą być również przez współpracujących z operatorem Serwisu
+            reklamodawców oraz partnerów. Szczegółowe informacje dotyczące
+            ustawień plików cookies oraz ich samodzielnego usuwania w
+            najpopularniejszych przeglądarkach internetowych powinny być
+            dostępne są w sekcji „pomoc” (lub innej podobnej) przeglądarki
+            internetowej oraz na poniższych stronach:
+            <ol className="list-decimal list-inside pl-6 mt-2">
+              <li>
+                przeglądarka Internet Explorer:
+                https://windows.microsoft.com/pl-pl/windows-10/edge-privacy-faq
+              </li>
+              <li>
+                przeglądarka Chrome:
+                https://support.google.com/chrome/answer/95647?hl=plX
+              </li>
+              <li>
+                przeglądarka Firefox:
+                https://support.mozilla.org/pl/kb/W%C5%82%C4%85czanie%20i%20wy%C5%82%C4%85czanie%20obs%C5%82ugi%20ciasteczek
+              </li>
+              <li>
+                przeglądarka Opera:
+                https://support.norton.com/sp/pl/pl/home/current/solutions/v57840314_NortonM_Retail_1_pl_pl
+              </li>
+              <li>
+                przeglądarka Microsoft Edge:
+                https://windows.microsoft.com/pl-pl/windows-10/edge-privacy-faq
+              </li>
+              <li>
+                przeglądarka Safari:
+                https://support.apple.com/kb/PH5042?locale=en_US
+              </li>
+            </ol>
+          </li>
+        </ul>
+      </section>
+      <section className="mb-6">
+        <h2 className="text-xl font-semibold mb-3">
+          BEZPIECZEŃSTWO DANYCH OSOBOWYCH
+        </h2>
+        <ul className="list-disc pl-5 space-y-2">
+          <li>
+            Administrator zapewnia odpowiednie środki bezpieczeństwa, w celu
+            zapewnienia, że Dane Osobowe przetwarzane są przez niego w sposób
+            bezpieczny, zapewniający przede wszystkim, że dostęp do danych mają
+            jedynie osoby upoważnione i jedynie w zakresie w jakim jest to
+            niezbędne ze względu na wykonywane przez nie zadania. Administrator
+            podejmuje wszelkie działania, aby podmioty współpracujące z nim
+            dawały gwarancję stosowania odpowiednich środków bezpieczeństwa, w
+            każdym przypadku gdy przetwarzają dane osobowe na zlecenie
+            Administratora. W szczególności są to następujące środki
+            bezpieczeństwa:
+            <ol className="list-decimal list-inside pl-6 mt-2">
+              <li>zabezpieczenie danych przed nieuprawnionym dostępem;</li>
+              <li>certyfikat SSL.</li>
+            </ol>
+          </li>
+        </ul>
+      </section>
+      <section className="mb-6">
+        <h2 className="text-xl font-semibold mb-3">DANE KONTAKTOWE</h2>
+        <p className="py-2">
+          W przypadku jakichkolwiek pytań, wniosków i próśb dotyczących Twoich
+          Danych Osobowych lub chęci skorzystania z określonego uprawnienia
+          możesz skontaktować się z Administratorem, w jednej z poniżej
+          wskazanych form:
+        </p>
+        <ul className="list-disc pl-5 space-y-2">
+          <li>
+            Krzysztof Humienny adres korenspodencyjny: Sojowa 29a/4 81-589
+            Gdynia
+          </li>
+          <li>e-mail: biuro@prezentowe-emocje.pl</li>
+        </ul>
+        <p className="py-2">
+          Administrator zastrzega sobie prawo realizacji Twoich uprawnień
+          związanych z przetwarzaniem Danych Osobowych po pozytywnym
+          zweryfikowaniu Twojej tożsamości.
+        </p>
+      </section>
+      <section className="mb-6">
+        <h2 className="text-xl font-semibold mb-3">
+          ZMIANA POLITYKI PRYWATNOŚCI
+        </h2>
+        <p className="py-2">
+          Administrator dąży do zapewnienia aktualności niniejszej Polityki
+          Prywatności i jej aktualizacji w przypadku zmiany przepisów prawa,
+          orzecznictwa sądowego, wytycznych organów odpowiedzialnych za nadzór
+          nad procesami przetwarzania danych osobowych, wprowadzenia Kodeksów
+          Dobrych Praktyk (jeżeli Administrator będzie związany takimi
+          kodeksami), zmiany technologii, sposobów, celów lub podstaw prawnych
+          przetwarzania Danych Osobowych.
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
